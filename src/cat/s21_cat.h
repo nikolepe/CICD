dec17d3e46fdfbb47e80f5a399c3cd294ce6debe
nikolepe <nikolepe@student.21-school.ru>
2024-09-14 15:47:01 +0000

    1

---
+++ src/cat/s21_cat.h
@@ -0,0 +1,23 @@
+#ifndef SRC_CAT_S21_CAT_H_
+#define SRC_CAT_S21_CAT_H_
+
+#include <getopt.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {  //структура хранит флаги
+  int flag_n;
+  int flag_b;
+  int flag_s;
+  int flag_t;
+  int flag_e;
+  int flag_v;
+  int countLines;
+  int countEmptyLines;
+
+} Flag;
+
+int WorkWithFile(const char *filename, Flag options);
+Flag ScanFlag(int argc, char *argv[]);
+
+#endif
